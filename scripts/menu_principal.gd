c71bbb1365624b0163626bdc56895a94e1821f0d
sslcristian <catencia1@udi.edu.co>
2026-05-20 21:09:15 -0500

    menu cinematico y musica implementados

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,14 +1,19 @@
 extends Control
 
-@onready var btn_iniciar : Button = $BtnIniciar
-@onready var btn_salir   : Button = $BtnSalir
+@onready var btn_iniciar: TextureButton = $BtnIniciar
+@onready var btn_salir: TextureButton = $BtnSalir
+@onready var menu_musica: AudioStreamPlayer = $MenuMusica
 
 func _ready() -> void:
 	btn_iniciar.pressed.connect(_on_iniciar)
 	btn_salir.pressed.connect(_on_salir)
+	menu_musica.play(3.0)
+
 
 func _on_iniciar() -> void:
+	menu_musica.stop()
 	get_tree().change_scene_to_file("res://scenes/Nivel1.tscn")
 
 func _on_salir() -> void:
+	menu_musica.stop()
 	get_tree().quit()
--- scripts/pantalla_final.gd
+++ scripts/pantalla_final.gd
@@ -1,14 +1,15 @@
 extends Control
 
-@onready var label_titulo  : Label  = $VBox/LabelTitulo
-@onready var label_mensaje : Label  = $VBox/LabelMensaje
-@onready var btn_menu      : Button = $VBox/BtnMenu
-@onready var btn_reiniciar : Button = $VBox/BtnReiniciar
+@onready var label_titulo: Label = $VBox/LabelTitulo
+@onready var label_mensaje: Label = $VBox/LabelMensaje
+@onready var btn_menu: Button = $VBox/BtnMenu
+@onready var btn_reiniciar: Button = $VBox/BtnReiniciar
 
 var _reiniciar_escena := "res://scenes/Nivel1.tscn"
 
 func _ready() -> void:
 	_configurar(false)
+
 	btn_menu.pressed.connect(_ir_menu)
 	btn_reiniciar.pressed.connect(_reiniciar)
 
@@ -18,16 +19,20 @@ func configurar(victoria: bool, reiniciar_escena := "res://scenes/Nivel1.tscn") 
 
 func _configurar(victoria: bool) -> void:
 	if victoria:
-		label_titulo.text  = "¡VICTORIA!"
+		label_titulo.text = "¡VICTORIA!"
 		label_mensaje.text = "Has derrotado a todos los robots.\n¡Las estructuras de datos te hicieron más fuerte!"
 		label_titulo.add_theme_color_override("font_color", Color.YELLOW)
 	else:
-		label_titulo.text  = "GAME OVER"
+		label_titulo.text = "GAME OVER"
 		label_mensaje.text = "El mundo postapocalíptico te venció.\n¡Estudia más y vuelve a intentarlo!"
 		label_titulo.add_theme_color_override("font_color", Color.RED)
 
 func _ir_menu() -> void:
+	get_tree().paused = false
+	queue_free()
 	get_tree().change_scene_to_file("res://scenes/MenuPrincipal.tscn")
 
 func _reiniciar() -> void:
+	get_tree().paused = false
+	queue_free()
 	get_tree().change_scene_to_file(_reiniciar_escena)
