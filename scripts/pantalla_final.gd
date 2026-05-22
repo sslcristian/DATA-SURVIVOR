56f886d6112d0749f5c9b5b3b5310b165a9280c8
sslcristian <catencia1@udi.edu.co>
2026-05-21 20:19:15 -0500

    pantalla final con botones y sonidos

--- scripts/pantalla_final.gd
+++ scripts/pantalla_final.gd
@@ -1,38 +1,68 @@
 extends Control
 
-@onready var label_titulo: Label = $VBox/LabelTitulo
-@onready var label_mensaje: Label = $VBox/LabelMensaje
-@onready var btn_menu: Button = $VBox/BtnMenu
-@onready var btn_reiniciar: Button = $VBox/BtnReiniciar
+@onready var img_victoria = $VBox/VICTORIA
+@onready var img_derrota = $VBox/DERROTA
+
+@onready var btn_menu = $BtnMenu
+@onready var btn_reiniciar = $BtnReiniciar
+
+@onready var sonido_victoria = $SonidoVictoria
+@onready var sonido_derrota = $SonidoDerrota
 
 var _reiniciar_escena := "res://scenes/Nivel1.tscn"
 
 func _ready() -> void:
-	_configurar(false)
+
+	# Ocultar imágenes al iniciar
+	img_victoria.visible = false
+	img_derrota.visible = false
 
 	btn_menu.pressed.connect(_ir_menu)
 	btn_reiniciar.pressed.connect(_reiniciar)
 
 func configurar(victoria: bool, reiniciar_escena := "res://scenes/Nivel1.tscn") -> void:
+
 	_reiniciar_escena = reiniciar_escena
 	_configurar(victoria)
 
 func _configurar(victoria: bool) -> void:
+
+	# Ocultar ambas
+	img_victoria.visible = false
+	img_derrota.visible = false
+
+	# Detener sonidos por seguridad
+	sonido_victoria.stop()
+	sonido_derrota.stop()
+
+	# Mostrar y reproducir
 	if victoria:
-		label_titulo.text = "¡VICTORIA!"
-		label_mensaje.text = "Has derrotado a todos los robots.\n¡Las estructuras de datos te hicieron más fuerte!"
-		label_titulo.add_theme_color_override("font_color", Color.YELLOW)
+
+		img_victoria.visible = true
+		sonido_victoria.play()
+
 	else:
-		label_titulo.text = "GAME OVER"
-		label_mensaje.text = "El mundo postapocalíptico te venció.\n¡Estudia más y vuelve a intentarlo!"
-		label_titulo.add_theme_color_override("font_color", Color.RED)
+
+		img_derrota.visible = true
+		sonido_derrota.play()
+
+func _detener_sonidos() -> void:
+
+	sonido_victoria.stop()
+	sonido_derrota.stop()
 
 func _ir_menu() -> void:
+
+	_detener_sonidos()
+
 	get_tree().paused = false
 	queue_free()
 	get_tree().change_scene_to_file("res://scenes/MenuPrincipal.tscn")
 
 func _reiniciar() -> void:
+
+	_detener_sonidos()
+
 	get_tree().paused = false
 	queue_free()
 	get_tree().change_scene_to_file(_reiniciar_escena)
